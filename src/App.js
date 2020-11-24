9fbd926e9b2852d21b8a067123d9844d0a3f754e
Zahid Maknojia <zahid_mak@outlook.com>
2020-11-23 20:06:37 -0500

    Using styled-components library to add css inside ` which then gets applied to compoentn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props=> props.alt ?  'red':'green'};
+  color:white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover{
+    background-color:${props=> props.alt?'salmon':'lightgreen'};
+    color:black;
+  }
+`
 
 class App extends Component {
   state = {
@@ -44,14 +59,14 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color:'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color:'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer'
+    // };
 
     let persons = null;
 
@@ -69,7 +84,7 @@ class App extends Component {
 
         </div>
       );
-      style.backgroundColor =  'red';
+      //style.backgroundColor =  'red';
     }
 
     const classes = [];
@@ -84,7 +99,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <button style={style} onClick={this.togglerPersonsHandler}>Toggle Perons</button>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglerPersonsHandler}>Toggle Perons</StyledButton>
         {persons}
       </div>
     );
