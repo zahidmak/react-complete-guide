277de3449d24ce058e9646df782d3e647c73cf19
Zahid Maknojia <zahid_mak@outlook.com>
2020-11-23 19:48:15 -0500

    Using styled-components library to add css inside ` which then gets applied to compoentn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
-
 
 class App extends Component {
   state = {
@@ -52,11 +50,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover':{
-        backgroundColor:'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -76,10 +70,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor =  'red';
-      style[':hover'] = {
-        backgroundColor:'lightred',
-        color:'black'
-      }
     }
 
     const classes = [];
@@ -91,16 +81,14 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p className={classes.join(' ')}>This is really working</p>
         <button style={style} onClick={this.togglerPersonsHandler}>Toggle Perons</button>
         {persons}
       </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import Radium from 'radium';
-import "./Person.css";
+import styled from 'styled-components';
 
+//import "./Person.css";
+const StyleDiv = styled.div`width: 60%;
+margin: 16px auto;
+border:1px solid #eee;
+box-shadow : 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+}
+@media (min-width:500px){
+    width:450px;
+}
+`;
 const person = (props) => {
-    const style={
-        '@media(min-width:500px)':{
-            width:'450px'
-        }
-    }
    
    return (
    
-    <div className="Person">
+    <StyleDiv>
         <p onClick={props.click}>I'm {props.name} Person and I am {props.age} year olds</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    </StyleDiv>
     
     );
 }
-export default Radium(person);
+export default person;
