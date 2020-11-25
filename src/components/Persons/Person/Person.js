105550c40593a4a676bae5f1222ba74b089ec15b
Zahid Maknojia <zahid_mak@outlook.com>
2020-11-24 21:54:43 -0500

    Lifecycle hooks. Looks in console for the order in which this lifecycle hooks are executed

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Person.css';
 
 
-const person = (props) => {
-    return (
+class Person extends Component {
+    // static getDerivedStateFromProps(props,state){
+    //     console.log('[Person.js] getDerivedStateFromProps');
+    //     return state;
+    // }
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('[Person.js] shouldComponentUpdate');
+        return true;
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('[Person.js] getSnapShotBeforeUpdate');
+        return null;
+    }
+
+    componentDidUpdate(){
+        console.log('[Person.js] componentDidUpdate')
+    }
+    render(){
+        console.log('[Person.js] rendering...');
+        return (
    
-    <div className={classes.Person}>
-        <p onClick={props.click}>I'm {props.name} Person and I am {props.age} year olds</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-    
-    );
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} Person and I am {this.props.age} year olds</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+            
+            );
+    }
+  
 }
-export default person;
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructure');
+  }
+
   state = {
     persons: [
       { id: "asd", name: "Manu", age: "29" },
@@ -13,7 +18,27 @@ class App extends Component {
     ]
   }
 
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps');
+    return state;
+  }
 
+  //This has been removed.
+  // componentWillMount(){
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
 
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -46,15 +71,8 @@ class App extends Component {
   }
 
   render() {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color:'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer'
-    // };
-
+    console.log('[App.js] render');
+   
     let persons = null;
     
     if (this.state.showPersons) {
