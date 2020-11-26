956c3ff1e8f15c5f14251955334df297471b0fef
Zahid Maknojia <zahid_mak@outlook.com>
2020-11-25 21:34:39 -0500

    Using React Fragment to render adjacents/multiple elements in the return statement inside render

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component. Fragment} from 'react';
 
 import classes from './Person.css';
 import Auxiliary from '../../../hoc/Auxiliary'
@@ -23,11 +23,11 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <Auxiliary>
+            <Fragment>
             <p key="i1" onClick={this.props.click}>I'm {this.props.name} Person and I am {this.props.age} year olds</p>
             <p key="i2">{this.props.children}</p>
             <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-            </Auxiliary>
+            </Fragment>
         )
    
            
