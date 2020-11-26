0efd388b02b993016307a6e9bda9b0ab89a22fe4
Zahid Maknojia <zahid_mak@outlook.com>
2020-11-25 21:58:36 -0500

    Another way of adding HOC components.This is javascript function (pure) based which is used to insert some additional javascript logic and has nothing to do with rendering etc.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Auxiliary from '../hoc/Auxiliary';
 
 class App extends Component {
   constructor(props) {
@@ -91,7 +92,7 @@ class App extends Component {
 
 
     return (
-      <WithClass classes={classes.App}>
+      <Auxiliary>
         <button onClick={() => {
           this.setState({ showCockpit: false });
         }}>Remove Cockpit</button>
@@ -102,9 +103,9 @@ class App extends Component {
         : null}
        
         {persons}
-      </WithClass>
+      </Auxiliary>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
